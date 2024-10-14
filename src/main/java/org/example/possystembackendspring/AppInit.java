f127fc80a5a9fb3998dba1cb27814b4f902801bf
loshaniramsha <loshanikottagoda@gmail.com>
2024-10-14 11:08:18 +0530

    Configure AppInit with root and servlet mappings

--- src/main/java/org/example/possystembackendspring/AppInit.java
+++ src/main/java/org/example/possystembackendspring/AppInit.java
@@ -1,4 +1,22 @@
 package org.example.possystembackendspring;
 
-public class AppInit {
+import org.example.possystembackendspring.config.WebAppConfig;
+import org.example.possystembackendspring.config.WebAppRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebAppRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
 }
