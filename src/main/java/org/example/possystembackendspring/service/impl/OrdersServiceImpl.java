c882bfd01e1aa42a66893cc9844e5cc244ad2b23
loshaniramsha <loshanikottagoda@gmail.com>
2024-10-16 01:46:02 +0530

    Add OrdersServiceImpl for order management operations Implemented methods to save, retrieve, and search orders Added method for generating unique order IDs

--- src/main/java/org/example/possystembackendspring/service/impl/OrdersServiceImpl.java
+++ src/main/java/org/example/possystembackendspring/service/impl/OrdersServiceImpl.java
@@ -1,4 +1,37 @@
 package org.example.possystembackendspring.service.impl;
 
-public class OrdersServiceImpl {
+import jakarta.transaction.Transactional;
+import org.example.possystembackendspring.dto.OrdersStates;
+import org.example.possystembackendspring.dto.impl.OrdersDTO;
+import org.example.possystembackendspring.service.OrdersService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@Transactional
+public class OrdersServiceImpl implements OrdersService {
+    @Override
+    public void saveOrder(OrdersDTO orderDTO) {
+
+    }
+
+    @Override
+    public List<OrdersDTO> getAllOrders() {
+        return List.of();
+    }
+
+    @Override
+    public OrdersStates getSelectedOrder(String orderId) {
+        return null;
+    }
+
+    @Override
+    public String generateOrderId() {
+        return "";
+    }
+
+    @Override
+    public List<OrdersDTO> searchByOrderId(String id) {
+        return List.of();
+    }
 }
