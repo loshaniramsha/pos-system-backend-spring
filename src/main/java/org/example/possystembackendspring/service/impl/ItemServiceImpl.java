bf22856d0325628d93818ea52b8069917d2b6869
loshaniramsha <loshanikottagoda@gmail.com>
2024-10-16 01:39:45 +0530

    Add ItemServiceImpl for item management operations Implemented methods for saving, updating, deleting, and retrieving items Added methods for searching by item code and generating item IDs

--- src/main/java/org/example/possystembackendspring/service/impl/ItemServiceImpl.java
+++ src/main/java/org/example/possystembackendspring/service/impl/ItemServiceImpl.java
@@ -1,4 +1,47 @@
 package org.example.possystembackendspring.service.impl;
 
-public class ItemServiceImpl {
+import jakarta.transaction.Transactional;
+import org.example.possystembackendspring.dto.ItemStates;
+import org.example.possystembackendspring.dto.impl.ItemDTO;
+import org.example.possystembackendspring.service.ItemService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@Transactional
+public class ItemServiceImpl implements ItemService {
+    @Override
+    public void saveItem(ItemDTO itemDTO) {
+
+    }
+
+    @Override
+    public void updateItem(String id, ItemDTO itemDTO) {
+
+    }
+
+    @Override
+    public void deleteItem(String id) {
+
+    }
+
+    @Override
+    public ItemStates getSelectedItem(String id) {
+        return null;
+    }
+
+    @Override
+    public List<ItemDTO> getAllItem() {
+        return List.of();
+    }
+
+    @Override
+    public String generateItemId() {
+        return "";
+    }
+
+    @Override
+    public List<ItemDTO> searchByItemCode(String newItemCode) {
+        return List.of();
+    }
 }
