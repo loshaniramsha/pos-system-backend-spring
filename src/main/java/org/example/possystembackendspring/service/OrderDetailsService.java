8b76fe6d7422f73d5561d4cf572af857e230a34f
loshaniramsha <loshanikottagoda@gmail.com>
2024-10-16 01:36:34 +0530

    Add OrderDetailsService interface for order detail management Added methods to save order details and retrieve details by order ID

--- src/main/java/org/example/possystembackendspring/service/OrderDetailsService.java
+++ src/main/java/org/example/possystembackendspring/service/OrderDetailsService.java
@@ -1,4 +1,10 @@
 package org.example.possystembackendspring.service;
 
+import org.example.possystembackendspring.dto.impl.OrderDetailsDTO;
+
+import java.util.List;
+
 public interface OrderDetailsService {
+    void saveOrderDetail(OrderDetailsDTO orderDetailDTO);
+    List<OrderDetailsDTO> getOrderDetails(String orderId);
 }
