8ef43b368dda144277a99aa611aedf371fa58933
loshaniramsha <loshanikottagoda@gmail.com>
2024-10-16 01:33:22 +0530

    Add CustomerService interface for customer management Methods for saving, updating, deleting, and fetching customers,Added search by contact and unique ID generation,Added method to retrieve selected customer state

--- src/main/java/org/example/possystembackendspring/service/CustomerService.java
+++ src/main/java/org/example/possystembackendspring/service/CustomerService.java
@@ -1,4 +1,16 @@
 package org.example.possystembackendspring.service;
 
+import org.example.possystembackendspring.dto.CustomerStates;
+import org.example.possystembackendspring.dto.impl.CustomerDTO;
+
+import java.util.List;
+
 public interface CustomerService {
+    void saveCustomer(CustomerDTO customerDTO);
+    void updateCustomer(String id, CustomerDTO customerDTO);
+    void deleteCustomer(String id);
+    CustomerStates getSelectedCustomer(String id);
+    List<CustomerDTO> getAllCustomers();
+    String generateCustomerId();
+    List<CustomerDTO> searchByContact(String value);
 }
