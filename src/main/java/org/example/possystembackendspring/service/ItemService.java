f571c4ee803fb90d43b73689cff9b320141a88ca
loshaniramsha <loshanikottagoda@gmail.com>
2024-10-16 01:35:21 +0530

    Add ItemService interface for item management. Added methods for saving, updating, and deleting items,Included item search, fetching all items, and unique ID generation,Added method to retrieve selected item state

--- src/main/java/org/example/possystembackendspring/service/ItemService.java
+++ src/main/java/org/example/possystembackendspring/service/ItemService.java
@@ -1,4 +1,16 @@
 package org.example.possystembackendspring.service;
 
+import org.example.possystembackendspring.dto.ItemStates;
+import org.example.possystembackendspring.dto.impl.ItemDTO;
+
+import java.util.List;
+
 public interface ItemService {
+    void saveItem(ItemDTO itemDTO);
+    void updateItem(String id, ItemDTO itemDTO);
+    void deleteItem(String id);
+    ItemStates getSelectedItem(String id);
+    List<ItemDTO> getAllItem();
+    String generateItemId();
+    List<ItemDTO> searchByItemCode(String newItemCode);
 }
