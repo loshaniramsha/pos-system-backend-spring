8e85d93c70e3492525eea53f8164f768e87f2141
loshaniramsha <loshanikottagoda@gmail.com>
2024-10-16 01:38:48 +0530

    Add CustomerServiceImpl with basic customer operations Implemented methods for saving, updating, deleting, and retrieving customers Added methods for searching by contact and generating customer IDs

--- src/main/java/org/example/possystembackendspring/service/impl/CustomerServiceImpl.java
+++ src/main/java/org/example/possystembackendspring/service/impl/CustomerServiceImpl.java
@@ -1,4 +1,47 @@
 package org.example.possystembackendspring.service.impl;
 
-public class CustomerServiceImpl {
+import jakarta.transaction.Transactional;
+import org.example.possystembackendspring.dto.CustomerStates;
+import org.example.possystembackendspring.dto.impl.CustomerDTO;
+import org.example.possystembackendspring.service.CustomerService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@Transactional
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public void saveCustomer(CustomerDTO customerDTO) {
+
+    }
+
+    @Override
+    public void updateCustomer(String id, CustomerDTO customerDTO) {
+
+    }
+
+    @Override
+    public void deleteCustomer(String id) {
+
+    }
+
+    @Override
+    public CustomerStates getSelectedCustomer(String id) {
+        return null;
+    }
+
+    @Override
+    public List<CustomerDTO> getAllCustomers() {
+        return List.of();
+    }
+
+    @Override
+    public String generateCustomerId() {
+        return "";
+    }
+
+    @Override
+    public List<CustomerDTO> searchByContact(String value) {
+        return List.of();
+    }
 }
