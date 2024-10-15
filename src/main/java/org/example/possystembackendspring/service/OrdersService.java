22f06f768fc1df26e47aa109e2b1d6a237df4b91
loshaniramsha <loshanikottagoda@gmail.com>
2024-10-16 01:37:39 +0530

    Add OrdersService interface for order management Added methods to save, retrieve, and search orders Included method for generating unique order IDs and fetching selected order state

--- src/main/java/org/example/possystembackendspring/service/OrdersService.java
+++ src/main/java/org/example/possystembackendspring/service/OrdersService.java
@@ -1,4 +1,14 @@
 package org.example.possystembackendspring.service;
 
+import org.example.possystembackendspring.dto.OrdersStates;
+import org.example.possystembackendspring.dto.impl.OrdersDTO;
+
+import java.util.List;
+
 public interface OrdersService {
+    void saveOrder(OrdersDTO orderDTO);
+    List<OrdersDTO> getAllOrders();
+    OrdersStates getSelectedOrder(String orderId);
+    String generateOrderId();
+    List<OrdersDTO> searchByOrderId(String id);
 }
