b6c0181ef56a504ef43d1c56d4c882b60f7afa8a
loshaniramsha <loshanikottagoda@gmail.com>
2024-10-16 01:45:09 +0530

    Add OrderDetailsServiceImpl for managing order details Implemented methods to save order details and retrieve details by order ID

--- src/main/java/org/example/possystembackendspring/service/impl/OrderDetailsServiceImpl.java
+++ src/main/java/org/example/possystembackendspring/service/impl/OrderDetailsServiceImpl.java
@@ -1,4 +1,21 @@
 package org.example.possystembackendspring.service.impl;
 
-public class OrderDetailsServiceImpl {
+import jakarta.transaction.Transactional;
+import org.example.possystembackendspring.dto.impl.OrderDetailsDTO;
+import org.example.possystembackendspring.service.OrderDetailsService;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@Transactional
+public class OrderDetailsServiceImpl implements OrderDetailsService {
+    @Override
+    public void saveOrderDetail(OrderDetailsDTO orderDetailDTO) {
+
+    }
+
+    @Override
+    public List<OrderDetailsDTO> getOrderDetails(String orderId) {
+        return List.of();
+    }
 }
