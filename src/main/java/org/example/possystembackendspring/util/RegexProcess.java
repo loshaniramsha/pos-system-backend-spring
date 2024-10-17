9770ea815a2caf722f9a5154eedd3409f49978e9
loshaniramsha <loshanikottagoda@gmail.com>
2024-10-17 13:09:53 +0530

    changes item regex

--- src/main/java/org/example/possystembackendspring/util/RegexProcess.java
+++ src/main/java/org/example/possystembackendspring/util/RegexProcess.java
@@ -1,9 +1,7 @@
 package org.example.possystembackendspring.util;
 
 public class RegexProcess {
-    /*public static final String CUSTOMER_ID_REGEX = "C\\d{2}-\\d{3}";*/
     public static final String CUSTOMER_ID_REGEX = "^C\\d{3}$";
-
     public static final String ITEM_ID_REGEX = "I\\d{2}-\\d{3}";
     public static final String ORDER_ID_REGEX = "OID-\\d{3}";
     public static final String CONTACT_REGEX = "^(?:\\+94|94|0)?7\\d{8}$";
